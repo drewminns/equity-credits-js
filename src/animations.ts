7f4b1b35de7ff7637baf1df78c298f2e38996dcd
Drew Minns <drew@playgroundinc.com>
2020-01-06 15:40:46 -0500

    Added ability to restart from pause

--- src/animations.ts
+++ src/animations.ts
@@ -7,10 +7,12 @@ export class Animations {
   SCROLL_TOP: number;
   PAGE_SCROLLING_PAUSED: boolean;
   SCROLL_ANIMATION: AnimeInstance | null;
+  SCROLL_POSITION: number;
 
   constructor() {
     this.SCROLL_ANIMATION = null;
     this.SCROLL_TOP = 0;
+    this.SCROLL_POSITION = 0;
     this.PAGE_HEIGHT = 0;
     this.PAGE_SCROLLING_PAUSED = false;
     this.SCROLL_ELEMENT = window.document.scrollingElement || window.document.body || window.document.documentElement;
@@ -23,22 +25,29 @@ export class Animations {
   init = (SCROLL_TOP: number) : void => {
     this.SCROLL_TOP = SCROLL_TOP;
     this.PAGE_HEIGHT = document.body.scrollHeight;
-    const intro = document.getElementById('intro')!;
-    const introText1 = document.getElementById('intro-text1')!;
-    const introText2 = document.getElementById('intro-text2')!;
+
 
     if (this.SCROLL_TOP === 0) {
       this.introAnimation();
     } else {
-      intro.style.opacity = '1';
-      introText1.style.opacity = '1';
-      introText2.style.opacity = '1';
-      this.scrollFadeIn();
-      this.pageScroll();
+      this.runScrolling()
     }
 
     const button = document.getElementById('play')!
     button.onclick = () => this.scrollControls();
+  }
+
+  private runScrolling = () => {
+    const intro = document.getElementById('intro')!;
+    const introText1 = document.getElementById('intro-text1')!;
+    const introText2 = document.getElementById('intro-text2')!;
+    intro.style.opacity = '1';
+    introText1.style.opacity = '1';
+    introText2.style.opacity = '1';
+
+    this.scrollFadeIn();
+    this.pageScroll();
+    this.navigationAnimation();
   }
 
   private introAnimation = () => {
@@ -62,8 +71,7 @@ export class Animations {
       })
 
     this.TIME_LINE.finished.then(() => {
-      this.scrollFadeIn();
-      this.pageScroll();
+      this.runScrolling();
     });
   }
 
@@ -82,7 +90,6 @@ export class Animations {
       duration: this.PAGE_HEIGHT / .009
     });
 
-    this.navigationAnimation();
   }
 
   private scrollControls = () => {
@@ -92,7 +99,7 @@ export class Animations {
       this.SCROLL_ANIMATION.pause();
       this.PAGE_SCROLLING_PAUSED = true;
     } else {
-      this.SCROLL_ANIMATION.play();
+      this.pageScroll();
       this.PAGE_SCROLLING_PAUSED = false;
     }
   }
