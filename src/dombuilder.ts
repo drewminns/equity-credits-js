f9bd7a431f8e5217796b44571d3137c8875b5b5c
Jenny <jenny@playgroundinc.com>
2020-01-07 16:21:59 -0500

    Open links in new tab

--- src/dombuilder.ts
+++ src/dombuilder.ts
@@ -88,7 +88,7 @@ export class DomBuilder {
 
           </span>
           <span class="${s.item_text}">
-            <a href="${item.shop_url}">${item.shop_name} <span class="${s.arrow}">${arrow}</span></a>
+            <a target="_blank" rel="noopener noreferrer" href="${item.shop_url}">${item.shop_name} <span class="${s.arrow}">${arrow}</span></a>
           </span>
         </p>
         ${ media }
