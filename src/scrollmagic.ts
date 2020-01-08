fd0c3275fd18fcad0a23060859ef236ee5697c0c
Jenny <jenny@playgroundinc.com>
2020-01-08 11:56:15 -0500

    Center images on mobile

--- src/scrollmagic.ts
+++ src/scrollmagic.ts
@@ -4,34 +4,46 @@ const ScrollMagic = require('scrollmagic');
 export class MagicTime {
   controller: any;
   scene: any;
+  PAGE_WIDTH: number;
+  BREAKPOINT: number;
+
+  constructor() {
+    this.PAGE_WIDTH = 0;
+    this.BREAKPOINT = 768;
+  }
 
   init = (section: any) => {
+    this.PAGE_WIDTH = document.body.scrollWidth;
 
-    this.controller = new ScrollMagic.Controller();
+    console.log(this.PAGE_WIDTH);
 
-    const wrapper = section.querySelector('[data-section-wrap]');
+    if (this.PAGE_WIDTH >= this.BREAKPOINT) {
+      this.controller = new ScrollMagic.Controller();
 
-    if (wrapper) {
-      const top = wrapper.getBoundingClientRect().top;
-      // Check if wrapper has images
-      const pin = wrapper.querySelector('.pin-me');
+      const wrapper = section.querySelector('[data-section-wrap]');
 
-      if (pin) {
+      if (wrapper) {
+        const top = wrapper.getBoundingClientRect().top;
+        // Check if wrapper has images
+        const pin = wrapper.querySelector('.pin-me');
 
-        const stopper = wrapper.querySelector('li[data-layout]');
-        const stopTop = stopper.getBoundingClientRect().top;
+        if (pin) {
 
-        const distance = stopTop - top - 262;
+          const stopper = wrapper.querySelector('li[data-layout]');
+          const stopTop = stopper.getBoundingClientRect().top;
 
-        if (distance >= 0) {
-          this.scene = new ScrollMagic.Scene({
-            triggerElement: section,
-            duration: distance,
-            triggerHook: 0.4,
-            reverse: false,
-          })
-            .setPin(pin, { pushFollowers: false })
-            .addTo(this.controller);
+          const distance = stopTop - top - 262;
+
+          if (distance >= 0) {
+            this.scene = new ScrollMagic.Scene({
+              triggerElement: section,
+              duration: distance,
+              triggerHook: 0.4,
+              reverse: false,
+            })
+              .setPin(pin, { pushFollowers: false })
+              .addTo(this.controller);
+          }
         }
       }
     }
