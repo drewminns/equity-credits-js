a06931c34b89f19f6c034b9a3ee16c45a2230a2e
Drew Minns <drew@playgroundinc.com>
2020-01-09 13:50:03 -0500

    removed cta from logo

--- src/components/logo.ts
+++ src/components/logo.ts
@@ -2,10 +2,8 @@ import clsx from 'clsx';
 
 import g from '../styles/_grid.scss';
 import s from '../styles/components/_logo.scss';
-import i from '../styles/components/_item.scss';
 
 import shopify_logo from '../assets/shopify_logo.svg';
-import arrow from '../assets/arrow.svg';
 
 export const logo = (() => `
   <div class="${g.row}">
@@ -14,12 +12,7 @@ export const logo = (() => `
         <div class="${clsx(s.logo_image)}">
           ${shopify_logo}
         </div>
-        <p class="${clsx(s.logo_text)}">Copyright 2019, Shopify Inc. All rights reserved.<br> If you want to start a store, click below to begin your free trial.</p>
-        <p class="${clsx(s.logo_cta)}">
-          <span class="${i.item_text}">
-            <a target="_blank" rel="noopener noreferrer" href="https://shopify.com">CTA <span class="${i.arrow}">${arrow}</span></a>
-          </span>
-        </p>
+        <p class="${clsx(s.logo_text)}">Copyright 2019, Shopify Inc. All rights reserved.</p>
       </div>
     </div>
   </div>
