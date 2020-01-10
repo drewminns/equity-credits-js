b0d64b51f07ec9623552eae7cff9487f2e2f0694
Jenny <jenny@playgroundinc.com>
2020-01-10 17:42:54 -0500

    Stick items in sections

--- src/scrollmagic.ts
+++ src/scrollmagic.ts
@@ -56,6 +56,29 @@ export class MagicTime extends Window {
               .setPin(pin, { pushFollowers: false })
               .addTo(this.CONTROLLER);
           }
+        } else if (pin) {
+
+          const { height, top } = this.SECTION.getBoundingClientRect();
+          const caption = pin.querySelector('figcaption');
+
+          let distance = height - 328;
+
+          if (caption) {
+            const captionHeight = caption.getBoundingClientRect().height;
+
+            distance = distance - captionHeight;
+          }
+
+          if (distance >= 0) {
+            this.SCENE = new ScrollMagic.Scene({
+              triggerElement: this.SECTION,
+              duration: distance,
+              triggerHook: 0.4,
+              reverse: false,
+            })
+              .setPin(pin, { pushFollowers: false })
+              .addTo(this.CONTROLLER);
+          }
         }
       }
     }
